c0c17f9ff42be550573c49ba87840671d22d42b5
Mattr91730-commits <Matt_r91730@yahoo.com>
2026-05-04 17:56:09 -0700

    Update StudentProfile.jsx

--- frontend/src/pages/teacher/StudentProfile.jsx
+++ frontend/src/pages/teacher/StudentProfile.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
-import { api } from "./lib/api";
+import { api } from "../../lib/api";
 import { useAuth } from "../../context/AuthContext";
 import { ArrowLeft, User, Briefcase } from "lucide-react";
 
