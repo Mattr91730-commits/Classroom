be0ed624a0758aa4210306f7ebe1ccd071f6823c
Mattr91730-commits <Matt_r91730@yahoo.com>
2026-05-06 21:23:32 -0700

    Update StudentProfile.jsx

--- frontend/src/pages/student/StudentProfile.jsx
+++ frontend/src/pages/student/StudentProfile.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { api } from "../../lib/api";
+import { api } from "@/lib/api";
 
 export default function StudentProfile() {
   const [data, setData] = useState(null);
