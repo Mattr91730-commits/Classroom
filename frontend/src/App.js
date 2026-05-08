932ac4645dcb677c7f28f49e20824be85852b10e
Mattr91730-commits <Matt_r91730@yahoo.com>
2026-05-08 16:46:10 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,7 +24,7 @@ function AppRouter() {
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="/student-login" element={<StudentLogin />} />
-      <Route path="/teacher/*" element={<div>Teacher Route Works</div>} />
+      <Route path="/teacher/*" element={<TeacherApp />} />
       <Route path="/student/*" element={<ProtectedStudent><StudentApp /></ProtectedStudent>} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
